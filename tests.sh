a511f9a8cd4416dd391146687125709789d1c134
matteotolloso <matteotolloso@gmail.com>
2023-07-28 00:36:23 +0200

    tests on scalability

--- tests.sh
+++ tests.sh
@@ -1,5 +1,5 @@
 
 IN_FILE="./dataset/1gb.txt"
-N_THREADS=32
+N_THREADS=1
 
-for i in {1..10}; do LD_PRELOAD=/home/m.tolloso/libs/lib/libjemalloc.so ./build/huff_thr $IN_FILE ./outputs/test_thr.txt  $N_THREADS ; done #|  grep "read and count" 
+for i in {1..10}; do LD_PRELOAD=/home/m.tolloso/libs/lib/libjemalloc.so ./build/huff_ff $IN_FILE ./outputs/test_ff.txt  $N_THREADS ; done #|  grep "read and count" 
