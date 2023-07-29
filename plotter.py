7d00429a4eccd9ced2eae1e445181708753cc088
matteotolloso <matteotolloso@gmail.com>
2023-07-29 15:11:02 +0200

    report

--- plotter.py
+++ plotter.py
@@ -1,16 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-seq_time = 566897566
+# seq_time = 566897566
 
-thr = np.loadtxt('efficiency_ff')
+thr = np.loadtxt('efficiency_thr')
+ff = np.loadtxt('efficiency_ff')
+
+thr = thr / 1e6
+ff = ff / 1e6
 
 
-for i in range(2, len(thr), 2):
-    thr[i] = (seq_time * i) / thr[i]
+# thr = seq_time/thr
+# ff = seq_time/ff
+
+# cores = [i for i in range(4,33,2)]
+
+# thr = thr / cores
+# ff = ff / cores
   
 
-plt.plot(thr)
+plt.scatter(range(4,33,2),thr, label='thread', marker='o')
+plt.scatter(range(4,33,2),ff, label='FastFlow', marker='v')
+
+plt.xticks(range(4,33,2))
+
+plt.xlabel('number of cores'), 
+plt.ylabel('time to complete')
+
+plt.legend()
 
 plt.savefig('efficiency.png')
 
