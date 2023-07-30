41b34335c4c87a57475fc467270327ec0bb23931
matteotolloso <matteotolloso@gmail.com>
2023-07-30 18:03:03 +0200

    new organization

--- general_test.sh
+++ general_test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-IN_FILE="./dataset/8gb.txt"
-N_THREADS=32
+IN_FILE="./dataset/8mb.txt"
+N_THREADS=1
 
 
 echo -e "\nTest with native threads\n" &&
-time LD_PRELOAD=/home/m.tolloso/libs/lib/libjemalloc.so ./build/huff_thr $IN_FILE ./outputs/test_thr.txt  $N_THREADS &&
+time  ./build/huff_thr $IN_FILE ./outputs/test_thr.txt  $N_THREADS &&
 echo -e "-----------------------------------\n" && 
 
 echo -e "\nTest with fastflow\n" &&
-time LD_PRELOAD=/home/m.tolloso/libs/lib/libjemalloc.so ./build/huff_ff $IN_FILE ./outputs/test_ff.txt  $N_THREADS &&
+time  ./build/huff_ff $IN_FILE ./outputs/test_ff.txt  $N_THREADS &&
 echo -e "-----------------------------------\n" &&
 
 echo -e "\nTest sequential\n" &&
-time LD_PRELOAD=/home/m.tolloso/libs/lib/libjemalloc.so ./build/huff_seq $IN_FILE ./outputs/test_seq.txt &&
+time ./build/huff_seq $IN_FILE ./outputs/test_seq.txt &&
 echo -e "-----------------------------------\n" &&
 
 diff ./outputs/test_ff.txt ./outputs/test_seq.txt &&
