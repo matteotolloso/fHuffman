11738443daf85509c5bdcfb0af27fc35b0e8120d
matteotolloso <matteotolloso@gmail.com>
2023-08-14 20:00:46 +0200

    tests

--- tests.sh
+++ tests.sh
@@ -1,10 +1,10 @@
 
 IN_FILE="./dataset/8gb.txt"
-N_THREADS=16
+N_THREADS=32
 
  # for i in {1..10}; do LD_PRELOAD=/home/m.tolloso/libs/lib/libjemalloc.so ./build/huff_seq $IN_FILE ./outputs/test_seq.txt ; done
 
 
-for i in {1..10} ; do
-    LD_PRELOAD=/home/m.tolloso/libs/lib/libjemalloc.so ./build/huff_ff $IN_FILE ./outputs/test_ff.txt  $N_THREADS ;
+for i in {25..33} ; do
+    LD_PRELOAD=/home/m.tolloso/libs/lib/libjemalloc.so ./build/huff_seq ./dataset/2_$i.txt ./outputs/test_ff.txt;
 done
